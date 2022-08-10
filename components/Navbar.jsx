e58bd0921f39ea7f86cba725336d33cab18678cc
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-08-10 19:20:17 +0700

    Added navbar shadow

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Navbar() {
   return(
-    <div className="bg-Hamagon flex justify-between items-center pr-5 text-white sticky top-0 z-10">
+    <div className="shadow-lg bg-Hamagon flex justify-between items-center pr-5 text-white sticky top-0 z-10">
       <Link href="/">
         <h1 className="text-2xl py-5 px-5 cursor-pointer">Navbar.</h1>
       </Link>
